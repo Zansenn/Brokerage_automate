8157ee62c71a787e10cfdc25ba8da3d07934ea86
Zannsen <nazz.sen@gmail.com>
2020-08-28 13:36:43 +0100

    Changed log in method, added arguments to find postcode function

--- Brokerage_automate.py
+++ Brokerage_automate.py
@@ -5,17 +5,22 @@ options = Options()
 options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 driver = webdriver.Chrome(chrome_options=options, executable_path="C:\\Users\\nazse\\chromedriver\\chromedriver.exe",)
 
+user_name = input(" what's your email address")
+pass_word = input("what's your password")
+
 def site_login():
-    driver.get("https://herefordshirebrokerage.care-for-it.com/index.php/login")
-    userID = driver.find_element_by_name("txtEmail")
-    userID.send_keys("email_address_here")
-    driver.find_element_by_name("txtPassword").send_keys("passwordhere")
-    driver.find_element_by_id("login_button").click()
+  driver.get("https://herefordshirebrokerage.care-for-it.com/index.php/login")
+  userID = driver.find_element_by_name("txtEmail")
+  userID.send_keys(user_name)
+  driver.find_element_by_name("txtPassword").send_keys(pass_word)
+  driver.find_element_by_id("login_button").click()
 
 site_login()
 
-def find_postcode():
+def find_postcode(postcode):
+  postcode = str(postcode)
   select_table = driver.find_element_by_css_selector('.col-sm-6:nth-child(2)')
-  select_table.find_element_by_partial_link_text("HR2").click()
- 
-find_postcode()
+  select_table.find_element_by_partial_link_text(postcode).click()
+
+
+#find_postcode('HR2')
